8b8bb6bdef95c170bbce63200ae8a9c9033b0126
Jared Markell <982083+jaredm4@users.noreply.github.com>
2020-05-23 12:45:04 -0700

    Added logging framework.

--- src/Command/HelloWorldCommand.php
+++ src/Command/HelloWorldCommand.php
@@ -2,6 +2,7 @@
 
 namespace Devops\Command;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -10,18 +11,29 @@ use Symfony\Component\Console\Output\OutputInterface;
 class HelloWorldCommand extends Command
 {
     protected static string $defaultName = 'test';
+    private LoggerInterface $logger;
 
-    protected function configure()
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
     {
         $this
             ->setDescription('Hello world sample to ensure configuration is setup correctly.')
             ->addOption('name', null, InputOption::VALUE_OPTIONAL, 'What is your name?', 'world');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $name = $input->getOption('name');
+        # Using $output should be rare. Use logger for normal output.
         $output->writeln("Hello, ${name}.");
+
+        $this->logger->warning('Test warning log.', [new \RuntimeException('Faux runtime exception.')]);
 
         return 0;
     }
--- tests/component/Command/HelloWorldCommandTest.php
+++ tests/component/Command/HelloWorldCommandTest.php
@@ -1,16 +1,27 @@
 <?php declare(strict_types=1);
 
 use Devops\Command\HelloWorldCommand;
+use Monolog\Handler\PsrHandler;
+use Monolog\Logger;
+use Monolog\Processor\UidProcessor;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
 
 final class HelloWorldCommandTest extends TestCase
 {
+    private Logger $logger;
+
+    protected function setUp(): void
+    {
+        $this->logger = new Logger('test', [new PsrHandler(new NullLogger())], [new UidProcessor()]);
+    }
+
     public function testOutputsHelloWorld(): void
     {
         $application = new Application();
-        $application->add(new HelloWorldCommand());
+        $application->add(new HelloWorldCommand($this->logger));
         $command = $application->find('test');
 
         $commandTester = new CommandTester($command);
@@ -23,7 +34,7 @@ final class HelloWorldCommandTest extends TestCase
     public function testOutputsHelloName(): void
     {
         $application = new Application();
-        $application->add(new HelloWorldCommand());
+        $application->add(new HelloWorldCommand($this->logger));
         $command = $application->find('test');
 
         $commandTester = new CommandTester($command);
