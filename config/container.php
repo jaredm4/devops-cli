07ea2ddba75e385f8ffe57c332a990b69897fc03
Jared Markell <982083+jaredm4@users.noreply.github.com>
2020-06-05 15:36:29 -0700

    Move the tag creation to the services file for simplicity.

--- config/container.php
+++ config/container.php
@@ -5,15 +5,12 @@ declare(strict_types=1);
 // helper file to return a usable container (used by devops and doctrine clis)
 
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 // Create the container
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->setParameter('root_dir', realpath(__DIR__.'/..'));
-// make container aware of console commands for autoconfigure
-$containerBuilder->registerForAutoconfiguration(Command::class)->addTag('console.command');
 $loader = new YamlFileLoader($containerBuilder, new FileLocator(__DIR__));
 $loader->load('services.yaml');
 $containerBuilder->compile();
